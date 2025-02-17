743f4f81be06e6aa59aaf697b0d752caadfcf362
Mireia <149465776+Miradriyo@users.noreply.github.com>
2025-02-17 22:14:26 +0000

    ejercicio 1 de apartado 4 sin acabar

---
+++ 4 Encapsulación y visibilidad/ejercicio1/src/App.java
@@ -0,0 +1,9 @@
+/*Diseña una clase CuentaBancaria que encapsule el comportamiento de una cuenta.
+ Debe tener los atributos privados titular y saldo, y los métodos públicos para depositar() 
+ y retirar() dinero, así como getSaldo() para consultar el saldo. 
+Implementa también un método mostrarInformacion() para imprimir el titular y el saldo de la cuenta. */
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ 4 Encapsulación y visibilidad/ejercicio1/src/CuentaBancaria.java
@@ -0,0 +1,38 @@
+public class CuentaBancaria {
+private String titular;
+private double saldo;
+
+public CuentaBancaria(String titular, double saldoInicial) {
+    this.titular = titular;
+    this.saldo = saldoInicial;
+}
+
+public void depositar(double cantidad){
+    if(cantidad>0){
+        saldo=saldo+cantidad;
+        System.out.println("depósito realizado. Saldo: "+ saldo);
+    }else{
+        System.out.println("dato erróneo. Depósito tiene que ser mayor que 0€");
+    }
+}
+
+public double retirar(double cantidad){
+    if (cantidad>0 && saldo >= cantidad) {
+        saldo=saldo-cantidad;
+        System.out.println("Cantidad retirada. Nuevo saldo: "+ saldo);
+        return saldo;
+    } else {
+        System.out.println("No hay saldo suficiente o la cantidad es inválida");
+        return saldo;
+        
+    }
+}
+
+public double getSaldo(){
+    return saldo;
+}
+
+public String getTitular(){
+    return titular;
+}
+}

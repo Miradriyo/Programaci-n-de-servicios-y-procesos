5ae9d80c3b1a9ecae1c03bdea52fa72219e52940
Mireia <149465776+Miradriyo@users.noreply.github.com>
2025-02-26 09:23:23 +0000

    Update App.java revisar en casa

--- 4 Encapsulación y visibilidad/ejercicio1/src/App.java
+++ 4 Encapsulación y visibilidad/ejercicio1/src/App.java
@@ -16,17 +16,30 @@ public class App {
         System.out.println("6. Salir");
         Scanner dato=new Scanner(System.in);
         int opcion=dato.nextInt();
+
+        ArrayList<CuentaBancaria> cuentasBancarias = new ArrayList<>(); // Creamos un ArrayList para guardar objetos CuentaBancaria
         switch (opcion) {
             case 1:
                 System.out.println("Introduzca el nombre del titular de la cuenta");
                 Scanner titular=new Scanner(System.in);
                 String nombre=titular.nextLine();
-                double saldo=0;
+                double saldo=0; //el sldo inicial es 0 porque acabamos de crear la cuenta
                 CuentaBancaria cuenta=new CuentaBancaria(nombre, saldo);
+                cuentasBancarias.add(cuenta) //agregamos la cuenta creada
+                System.out.println("Cuenta bancaria creada a nombre de: "+ nombre);
+                titular.close();
                 break;
-            case 2:
+            case 2: //pedir al usuario que elija cual quiere usar, pedirle cantidad a depositar y realizar el depósito
+                System.out.println("Introduzca el nombre de la cuenta");
+                Scanner titular= new Scanner(SysteScannerm.in);
+                String nombreTitular=titular.nextLine();
+                for (CuentaBancaria cuenta:cuentasBancarias){ //Recorremos la lista de cuentas
+                    if()
+                }
+                double deposito.depositar();
             default:
                 break;
         }
+      }
     }
 }
